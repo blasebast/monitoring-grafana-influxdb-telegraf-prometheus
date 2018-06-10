2a35408867002643c83cb9effbc373ed6660c57e
seba <sebastian.blasiak@gmail.com>
2018-06-10 16:05:43 +0200

    adding docker* installation

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,6 +1,23 @@
+## INSTALL docker-ce
+read -r -p "Do you want to install docker? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+	curl -fsSL get.docker.com -o get-docker.sh
+	sudo sh get-docker.sh
+	sudo usermod -aG docker $(whoami)
+else
+	echo -e "'no' chosen for docker installation - docker assummed to be already installed"
+fi
+
 ## INSTALL docker-compose
-## uncomment if you don't have docker-compose installed  #sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-## uncomment if you don't have docker-compose installed  #sudo chmod +x /usr/local/bin/docker-compose
+read -r -p "Do you want to install docker-compose? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+	sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+else
+	echo -e "'no' chosen for docker-compose installation - docker-compose assummed to be already installed"
+fi
 
 # START docker-compose
 docker-compose up -d 
